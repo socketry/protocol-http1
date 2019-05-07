c84279fe8b2d60f9daf73b261c8642976a5783e5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-05-07 22:23:30 +1200

    Migration of code from http-protocol.

---
+++ lib/protocol/http1.rb
@@ -0,0 +1,8 @@
+require "protocol/http1/version"
+
+module Protocol
+  module HTTP1
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/protocol/http1/version.rb
@@ -0,0 +1,5 @@
+module Protocol
+	module HTTP1
+		VERSION = "0.1.0"
+	end
+end
