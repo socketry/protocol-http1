a776c789dc92c2385fa1afef563760731fe9f98a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-04-18 15:11:52 +1200

    Initial fuzzing.

---
+++ fuzz/request/bake.rb
@@ -0,0 +1,5 @@
+
+# Run the fuzz test.
+def run
+	system("AFL_SKIP_BIN_CHECK=1 afl-fuzz -i input/ -o output/ -t 1000  -m 1000 -- ruby script.rb")
+end
---
+++ fuzz/request/script.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+require_relative '../../lib/protocol/http1'
+
+def test
+	# input, output = Socket.pair(Socket::PF_UNIX, Socket::SOCK_STREAM)
+	
+	server = Protocol::HTTP1::Connection.new($stdin)
+	
+	# input.write($stdin.read)
+	# input.close
+
+	begin
+		host, method, path, version, headers, body = server.read_request
+		
+		body = server.read_request_body(method, headers)
+	rescue Protocol::HTTP1::InvalidRequest
+		# Ignore.
+	end
+end
+
+if ENV["_"] =~ /afl/
+	require 'kisaten'
+	Kisaten.crash_at [], [], Signal.list['USR1']
+	
+	while Kisaten.loop 10000
+		test
+	end
+else
+	test
+end
