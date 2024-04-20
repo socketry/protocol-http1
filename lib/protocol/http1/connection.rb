4d6cc7bd6c1610ac97674975080b0f896b745d9e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-20 22:24:13 +1200

    Modernize gem.

--- lib/protocol/http1/connection.rb
+++ lib/protocol/http1/connection.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2019-2023, by Samuel Williams.
+# Copyright, 2019-2024, by Samuel Williams.
 # Copyright, 2019, by Brian Morearty.
 # Copyright, 2020, by Bruno Sutic.
 # Copyright, 2023, by Thomas Morgan.
